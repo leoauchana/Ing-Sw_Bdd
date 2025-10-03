b69e40571602055839574bc265f142c5aa90b9e0
Leoauchana <matiasleonelcja@gmail.com>
2025-10-03 16:17:26 -0300

    Creacion de carpetas

---
+++ IngSw_Bdd/Features/ModuloUrgencias.feature
@@ -0,0 +1,8 @@
+Feature: Módulo de Urgencias
+
+    Feature Description
+
+    Scenario: Ingreso de un paciente a la lista de espera de urgencias
+        Given que la mili esta enamorada de leo
+        When leo le dice algo lindo
+        Then mili se baja la bombacha
---
+++ IngSw_Bdd/StepDefinitions/ModuloDeUrgenciasStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using Reqnroll;
+
+namespace IngSw_Bdd.StepDefinitions
+{
+    [Binding]
+    public class ModuloDeUrgenciasStepDefinitions
+    {
+        [Given("")]
+        public void Given()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("")]
+        public void When()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("")]
+        public void Then()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
